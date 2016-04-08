1ce87f55e58aae5ef0d7e322759ca8c8e3f63cc4
Spencer Krum <nibz@spencerkrum.com>
2016-04-07 22:57:14 -0700

    Handle hash and array lookups

--- lib/hiera/backend/garbage_backend.rb
+++ lib/hiera/backend/garbage_backend.rb
@@ -9,12 +9,22 @@ class Hiera
       end
 
       def lookup(key, scope, order_override, resolution_type)
-        answer = 'garbage'
 
+        case resolution_type.is_a?(Hash) ? :hash : resolution_type
+        when :array
+          answer ||= []
+          answer << "garbage"
+        when :hash
+          answer ||= {}
+          answer['garbage'] = 'garbage'
+        else
+          answer = "garbage"
+          break
+        end
         Hiera.debug(answer)
 
         return answer
-
+        end
       end
     end
   end
